ea3851a84d769df88503e6fc54793a4b31af05de
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-11-11 23:31:22 +0100

    surveillance ctrl

---
+++ app/Http/Controllers/SurveillanceController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SurveillanceController extends Controller
+{
+    // offender & stolen cars will be reason surveiilance
+}
