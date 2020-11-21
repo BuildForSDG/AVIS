63ccc8afb69537b4a38e521098078dc6f96cbf16
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2020-11-21 19:35:15 +0100

    added footer inc

---
+++ resources/views/inc/footer.php
@@ -0,0 +1,7 @@
+ <footer class="page-footer font-small blue">
+      
+
+        <div align="center" style="color: green"><a href="http://twitter.com/@tobinocycle"> Allrights Reserved, AVIS 2020. Tobaino </a></div>
+        <!-- Copyright -->
+
+    </footer>
